ce25aa1858772e006895b9ede94d3cf66741792d
anas.r.firdousi <anas.firdousi@gmail.com>
2017-06-21 01:18:17 -0700

    Added example for optimized stream management

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log('*** Multiple Streams - Naive Example with no optimization ***');
 var myGenericComponent = {
 
   updateData: function(data) {
-    console.log('Updating data with value ', data);
+    console.log('Naive non-optimized example: Value ', data);
   },
 
   onMount: function() {
@@ -28,7 +28,7 @@ var myGenericComponent = {
                         .subscribe((value) => {
                           console.log(value);
                           if (+value > 75) {
-                            console.log('Unsubcribing because recorded value was > 75');
+                            console.log('Unsubscribing naive non-optimized because value > 75');
                             //this.dataSub.unsubscribe();
                             this.onUnmountRange(); // Alternate example, show line above
                           }
@@ -49,10 +49,44 @@ var myGenericComponent = {
   }
 };
 
-//window.onload =
 myGenericComponent.onMount();
 
 // Also call myGenericComponent.onUnmount() for learning
 
 console.log('*** Multiple Streams - Naive Example with optimization ***');
 
+var myOptimizedGenericComponent = {
+
+  updateData: function(data) {
+    console.log('Naive optimized example: Value ', data);
+  },
+
+  onMount: function() {
+
+    // DOM References
+    const cancelBtn = document.querySelector('#btnCancelOptimized');
+    const rangeSelector = document.querySelector('#inRangeOptimized');
+
+    // Source Streams
+    let data$ = this.getData();
+    let cancel$ = Rx.Observable.fromEvent(cancelBtn, 'click');
+    let range$ = Rx.Observable.fromEvent(rangeSelector, 'change').map(e => e.target.value);
+
+    // Merge Stream
+    const stop$ = Rx.Observable.merge(cancel$, range$.filter(x => x> 75));
+
+    // Subscription
+    this.subscription = data$.takeUntil(stop$).subscribe( data => this.updateData(data));
+
+  },
+
+  onUnmount: function() {
+    this.subscription.unsubscribe();
+  },
+
+  getData(){
+    return  Rx.Observable.interval(1000);
+  }
+};
+
+myOptimizedGenericComponent.onMount();
